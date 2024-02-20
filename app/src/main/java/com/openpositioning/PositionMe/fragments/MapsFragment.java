f941f8d372c2cc40a65b7ee9c1280f808df618ae
arsalansiddique1 <arsalan1699@hotmail.com>
2024-02-20 03:26:59 +0000

    Features 2a-2d seem to be working. Testing current features to see if all building floor plans are alligned correctly and switch without issues

--- app/src/main/java/com/openpositioning/PositionMe/fragments/MapsFragment.java
+++ app/src/main/java/com/openpositioning/PositionMe/fragments/MapsFragment.java
@@ -38,24 +38,24 @@ import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.maps.android.PolyUtil;
-import com.google.common.collect.Maps;
+
 import com.openpositioning.PositionMe.R;
 import com.openpositioning.PositionMe.sensors.SensorFusion;
 import com.openpositioning.PositionMe.sensors.SensorTypes;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import android.util.Log;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MapsFragment extends Fragment {
     private GoogleMap mMap;
-    private List<LatLng> pathPoints = new ArrayList<>();
     private Polyline currentPath;
-    private Marker userMarker;
     private SensorFusion sensorFusion =  SensorFusion.getInstance();
     private Handler handler = new Handler();
     float[] startPosition = sensorFusion.getGNSSLatitude(true);
@@ -76,6 +76,13 @@ public class MapsFragment extends Fragment {
     // Create a new HashMap or similar structure to hold your building names and PolygonOptions
     HashMap<String, PolygonOptions> buildingPolygons = new HashMap<>();
 
+    private int currentFloor = 0; // default ground floor
+    private String currentBuildingId = ""; // ID of the current building being viewed
+    private Map<String, Integer []> buildingFloors; // mapping building ID to number of floors
+    // Define variables for storing building floors and displaying them
+    private ImageButton buttonUp;
+    private ImageButton buttonDown;
+
     private GroundOverlay currentGroundOverlay = null; // Field to keep track of the current overlay
 
     //Used to get distance error between GNSS and PDR.
@@ -84,6 +91,9 @@ public class MapsFragment extends Fragment {
     private CountDownTimer autoStop;
     //Used for converting displacements to latitude,longitudes
     static final double earthRadius = 6378137; // Radius in meters
+
+
+
 
 
     private final OnMapReadyCallback callback = new OnMapReadyCallback() {
@@ -114,6 +124,9 @@ public class MapsFragment extends Fragment {
             mMap.setMyLocationEnabled(true);
             //Loads polygons onto the map for building coordinates defined within the function.
             initializeAndAddPolygons();
+            //Loads data for building floors
+            initializeBuildingFloors();
+
             //Starts displaying live path and orientation once map is ready.
             handler.post(pathUpdater);
         }
@@ -132,7 +145,7 @@ public class MapsFragment extends Fragment {
         }
     }
 
-    //function used to convert the compass icon to a bitmap so it can be used as a marker on maps.
+    //Helper function used to convert the compass icon to a bitmap so it can be used as a marker on maps.
     private BitmapDescriptor bitmapDescriptorFromVector(Context context, @DrawableRes int vectorDrawableResourceId) {
         Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorDrawableResourceId);
         vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
@@ -301,6 +314,19 @@ public class MapsFragment extends Fragment {
             mMap.addPolygon(entry.getValue()); // Add the polygon to the map
         }
     }
+    //Stores and initialises building floors
+    private void initializeBuildingFloors() {
+        buildingFloors = new HashMap<>();
+        // Add buildings and their respective number of floors including ground and lower ground if any
+        buildingFloors.put("Nucleus", new Integer[]{-1, 0, 1, 2, 3}); //include lower ground as a floor
+        buildingFloors.put("MurrayLibrary", new Integer[]{0, 1, 2, 3}); // Adjust the floor count as necessary
+        buildingFloors.put("FleemingJenkin", new Integer[]{0, 1}); // and so on for other buildings
+        buildingFloors.put("HudsonBeare", new Integer[]{0, 1});
+        buildingFloors.put("Sanderson", new Integer[]{0, 1, 2});
+        // Add other buildings as necessary
+    }
+
+
     // Method to check if user is inside any building and switch to indoor map
     public void checkUserLocationAndUpdateMap(LatLng currentUserLocation) {
         boolean foundBuilding = false;
@@ -325,6 +351,9 @@ public class MapsFragment extends Fragment {
         if (!foundBuilding && currentGroundOverlay != null) {
             currentGroundOverlay.remove();
             currentGroundOverlay = null; // Clear the reference to prevent memory leaks
+            // Make the floor navigation buttons disappear.
+            buttonUp.setVisibility(View.GONE);
+            buttonDown.setVisibility(View.GONE);
         }
     }
     // Method to switch to an indoor map based on the buildingId
@@ -334,15 +363,96 @@ public class MapsFragment extends Fragment {
         // Define GroundOverlayOptions for the indoor map
         GroundOverlayOptions indoorMapOverlay = new GroundOverlayOptions();
 
-        // Example to set the overlay - Adjust location, image, and dimensions as necessary
+        // Sets indoor map overlay to the current floor the user has selected, at run time this is 0.
         indoorMapOverlay.positionFromBounds(getBoundsFromPolygon(buildingId)); // Assuming you have bounds for each building
-        indoorMapOverlay.image(BitmapDescriptorFactory.fromResource(getIndoorMapResource(buildingId))); // Method to get the correct resource
+        indoorMapOverlay.image(BitmapDescriptorFactory.fromResource(getIndoorMapResource(buildingId, currentFloor))); // Method to get the correct resource
 
 
         // Add the overlay to the map
         currentGroundOverlay = mMap.addGroundOverlay(indoorMapOverlay);
         currentGroundOverlay.setTag(buildingId);
+        currentBuildingId = buildingId;
+
+
+        // Make the floor navigation buttons visible
+        buttonUp.setVisibility(View.VISIBLE);
+        buttonDown.setVisibility(View.VISIBLE);
+
+        // Display a toast with the floors available for this building
+        Integer[] floors = buildingFloors.get(buildingId);
+        if (floors != null) {
+            String availableFloors = "Floors available for " + buildingId + ": " + Arrays.toString(floors)
+                    .replace("[", "")
+                    .replace("]", "");
+            Toast.makeText(getContext(), availableFloors, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getContext(), "No floor information available for " + buildingId, Toast.LENGTH_SHORT).show();
+        }
+
     }
+
+
+    // Helper method to get the resource ID for a specific floor of a building
+    private int getIndoorMapResource(String buildingId, int floor) {
+        // Return the resource ID for the specified floor of the building
+        // You'll need to map floor numbers to resource IDs
+        switch (buildingId) {
+            case "Nucleus":
+                if (floor == -1) return R.drawable.nucleuslg;
+                else if (floor == 0) return R.drawable.nucleusg;
+                else if (floor == 1) return R.drawable.nucleus1;
+                else if (floor == 2) return R.drawable.nucleus2;
+                else if (floor == 3) return R.drawable.nucleus3;
+            case "MurrayLibrary":
+                if (floor == 0) return R.drawable.libraryg;
+                else if (floor == 1) return R.drawable.library1;
+                else if (floor == 2) return R.drawable.library2;
+                else if (floor == 3) return R.drawable.library3;
+            case "FleeminJenkin":
+                if (floor == 0) return R.drawable.fleeming_jenkin1;
+                else if (floor == 1) return R.drawable.fleeming_jenkin2;
+            case "HudsonBeare":
+                if (floor == 0) return R.drawable.hudson_beare1;
+                else if (floor == 1) return R.drawable.hudson_beare2;
+            case "Sanderson":
+                if (floor == 0) return R.drawable.sanderson1;
+                else if (floor == 1) return R.drawable.sanderson2;
+                else if (floor == 2) return R.drawable.sanderson3;
+                // ... other buildings ...
+            default:
+                return -1; // Invalid resource
+        }
+    }
+
+    //Helper function which Removes the existing overlay if it exists.
+    //Retrieves the resource ID for the new floor's map based on the current building and floor.
+    //Creates a new overlay with the floor map and adds it to the map.
+    private void updateIndoorMapForCurrentFloor() {
+        // Check if there's an existing ground overlay and remove it
+        if (currentGroundOverlay != null) {
+            currentGroundOverlay.remove();
+        }
+
+        int floorMapResourceId = getIndoorMapResource(currentBuildingId, currentFloor);
+
+        // Check if a valid floor map resource ID was found
+        if (floorMapResourceId != -1) {
+            // Define new GroundOverlayOptions for the new floor
+            GroundOverlayOptions newFloorOverlayOptions = new GroundOverlayOptions()
+                    .positionFromBounds(getBoundsFromPolygon(currentBuildingId)) // Assuming this method exists and works as before
+                    .image(BitmapDescriptorFactory.fromResource(floorMapResourceId));
+
+            // Add the overlay to the map and keep a reference to it
+            currentGroundOverlay = mMap.addGroundOverlay(newFloorOverlayOptions);
+            currentGroundOverlay.setTag(currentBuildingId); // Tag the overlay with the building ID
+        } else {
+            // Optionally, handle the case where no valid floor map was found
+            Log.e("MapUpdate", "No valid floor map found for building: " + currentBuildingId + ", floor: " + currentFloor);
+        }
+    }
+
+
+
     //Helper function to find LatLng bounds from the set of polygon points
     public LatLngBounds getBoundsFromPolygon(String buildingId) {
         PolygonOptions polygonOptions = buildingPolygons.get(buildingId);
@@ -358,34 +468,6 @@ public class MapsFragment extends Fragment {
 
         return builder.build();
     }
-
-    // Helper method to get the resource ID of the indoor map based on the buildingId
-    // Adjust this method to return the correct floor map resource for each building
-    private int getIndoorMapResource(String buildingId) {
-        switch (buildingId) {
-            case "Nucleus":
-                return R.drawable.nucleusg;
-            case "MurrayLibrary":
-                return R.drawable.libraryg;
-            case "FleemingJenkin":
-                return R.drawable.fleeming_jenkin1;
-            case "HudsonBeare":
-                return R.drawable.hudson_beare1;
-            case "Sanderson":
-                return R.drawable.sanderson1;
-            // Add cases for other buildings
-            default:
-                return -1; // Invalid resource
-        }
-    }
-
-
-
-
-
-
-
-
 
 
     @Nullable
@@ -466,6 +548,70 @@ public class MapsFragment extends Fragment {
                 mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
             }
         });
+
+        // Instantiate and set onClick behaviour for indoor floor plan selection buttons
+        buttonUp = (ImageButton) getView().findViewById(R.id.upButton);
+        this.buttonUp.setOnClickListener(new View.OnClickListener() {
+            /**
+             * {@inheritDoc}
+             * OnClick listener for button to set indoor map to go up a floor.
+             * When button clicked the indoor map overlay displayed changes by +ve.
+             */
+            @Override
+            public void onClick(View view) {
+                Integer[] floors = buildingFloors.get(currentBuildingId);
+                if (floors != null) {
+                    // Find the index of the current floor within the floors array
+                    int currentFloorIndex = Arrays.binarySearch(floors, currentFloor);
+                    // Check if there's a next floor within the array bounds
+                    if (currentFloorIndex >= 0 && currentFloorIndex < floors.length - 1) {
+                        // Set the next floor as the current floor
+                        currentFloor = floors[currentFloorIndex + 1];
+                        // Update the indoor map to reflect the new floor
+                        updateIndoorMapForCurrentFloor();
+                        Toast.makeText(getContext(), "Switched to floor " + currentFloor, Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Optionally notify the user that they are on the top floor
+                        Toast.makeText(getContext(), "You are on the top floor.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+        buttonDown = (ImageButton) getView().findViewById(R.id.downButton);
+        this.buttonDown.setOnClickListener(new View.OnClickListener() {
+            /**
+             * {@inheritDoc}
+             * OnClick listener for button to go to set indoor map to go down a floor.
+             * When button clicked the indoor map overlay displayed changes by -ve.
+             */
+            @Override
+            public void onClick(View view) {
+                Integer[] floors = buildingFloors.get(currentBuildingId);
+                if (floors != null) {
+                    // Find the index of the current floor within the floors array
+                    int currentFloorIndex = Arrays.binarySearch(floors, currentFloor);
+                    // Check if there's a next floor within the array bounds
+                    if (currentFloorIndex > 0) {
+                        // Set the next floor as the current floor
+                        currentFloor = floors[currentFloorIndex - 1];
+                        // Update the indoor map to reflect the new floor
+                        updateIndoorMapForCurrentFloor();
+                        // Optionally, show a toast with the new floor information
+                        Toast.makeText(getContext(), "Switched to floor " + currentFloor, Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Optionally, inform the user they are at the lowest floor already
+                        Toast.makeText(getContext(), "You are at the lowest floor available.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+
+
+
 
 
 
